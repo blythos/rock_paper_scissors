0ff6687e7aae7b9085b13c87773149e49ff0847b
Stephen Blythe <stephenblythe@gmail.com>
2019-08-05 20:44:38 +0100

    Adds homepage and some CSS

--- models/Game.rb
+++ models/Game.rb
@@ -7,19 +7,19 @@ class Game
 
   def play()
     if @hand1 == @hand2
-      return "tie"
+      return "You both play #{@hand1}. It's a tie!"
     elsif @hand1 == "rock" && @hand2 == "scissors"
-      return @hand1
+      return "You win with #{@hand1}! Comnputer loses with #{@hand2}."
     elsif @hand1 == "scissors" && @hand2 == "paper"
-      return @hand1
+      return "You win with #{@hand1}! Computer loses with #{@hand2}"
     elsif @hand1 == "paper" && @hand2 == "rock"
-      return @hand1
+      return "You win with #{@hand1}! Computer loses with #{@hand2}"
     elsif @hand2 == "rock" && @hand1 == "scissors"
-      return @hand2
+      return "Computer wins with #{@hand2}! You lose with #{@hand1}"
     elsif @hand2 == "scissors" && @hand1 == "paper"
-      return @hand2
+      return "Computer wins with #{@hand2}! You lose with #{@hand1}"
     elsif @hand2 == "paper" && @hand1 == "rock"
-      return @hand2
+      return "Computer wins with #{@hand2}! You lose with #{@hand1}"
     end
   end
 

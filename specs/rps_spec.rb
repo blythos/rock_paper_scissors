c0623cadc87b81fa33889610d271f4d61fe961fe
Stephen Blythe <stephenblythe@gmail.com>
2019-08-05 16:17:43 +0100

    Tests all possible outcomes.

--- specs/rps_spec.rb
+++ specs/rps_spec.rb
@@ -9,7 +9,9 @@ class TestRps < MiniTest::Test
     @game2 = Rps.new("Rock", "Scissors")
     @game3 = Rps.new("Rock", "Rock")
     @game4 = Rps.new("Paper", "Rock")
-    @game5 = Rps.new("Rock", "Scissors")
+    @game5 = Rps.new("Scissors", "Rock")
+    @game6 = Rps.new("Paper", "Scissors")
+    @game7 = Rps.new("Rock", "Paper")
   end
 
   def test_player1_rock_wins()
@@ -30,5 +32,13 @@ class TestRps < MiniTest::Test
 
   def test_player2_rock_wins()
     assert_equal("rock", @game5.play())
+  end
+
+  def test_player2_scissors_wins()
+    assert_equal("scissors", @game6.play())
+  end
+
+  def test_player_2_paper_wins()
+    assert_equal("paper", @game7.play())
   end
 end
